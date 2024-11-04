811061e5bd851fed0f3e0605e4430ea9126f5702
Duke Nguyen <60281815+ImmenseHorse@users.noreply.github.com>
2024-11-04 15:05:25 -0500

    Fix Lexicon

--- Assignment 4/bankAccounts.pl
+++ Assignment 4/bankAccounts.pl
@@ -17,7 +17,6 @@
 %%%%% SECTION: database
 %%%%% Put statements for account, created, lives, location and gender below
 
-
 account(12,ann,metro_credit_union,4505).
 account(13,robert,rbc,750).
 account(14, philip, cibc, 4050).
@@ -32,7 +31,6 @@ account(22, mia, western_bank, 12342).
 account(23, james, wellsFargo, 31313).
 account(24, tom, bank_of_montreal, 100).
 account(25, connor, rbc, 100).
-
 
 created(12, ann, metro_credit_union, 3, 2020).
 created(13, robert, rbc, 7, 1996).
@@ -49,8 +47,6 @@ created(23, james, wellsFargo, 11, 1999).
 created(24, tom, bank_of_montreal, 3, 1997).
 created(25, connor, rbc, 4, 2024).
 
-
-
 lives(ann, markham).
 lives(robert, losAngeles).
 lives(philip, richmondHill).
@@ -65,9 +61,6 @@ lives(james, chicago).
 lives(tom, scarborough).
 lives(connor, london).
 
-
-
-
 location(scarborough, canada). 
 location(markham, canada).
 location(richmondHill, canada).
@@ -81,9 +74,6 @@ location(chicago, usa).
 location(london, united_kingdom).
 location(toronto, canada).
 location(losAngeles, usa).
-
-
-
 
 location(rbc,toronto).
 location(metro_credit_union, scarborough).
@@ -110,9 +100,6 @@ gender(james, man).
 gender(tom, man).
 gender(connor, man).
 
-
-
-
 %%%%% SECTION: lexicon
 %%%%% Put the rules/statements defining articles, adjectives, proper nouns, common nouns,
 %%%%% and prepositions in this section.
@@ -123,35 +110,29 @@ gender(connor, man).
 %%%%% DO NOT INCLUDE ANY statements for account, created, lives, location and gender 
 %%%%%     in this section
 
-%%%%% helpers 
 
+%%%%% helpers
 bank(X) :- account(_, _, X, _).
-
-person(P) :- gender(P, man).
-person(P) :- gender(P, woman).
-
+person(X) :- lives(X, _).
 man(X) :- gender(X, man).
 woman(X) :- gender(X, woman).
-
 city(X) :- lives(_, X).
+country(X) :- location(_, X), not city(X).
 
-country(X) :- location(_, X), \+ city(X).
 
 %%%%% article
-
 article(a).
 article(an).
 article(the).
 article(any).
 
-%%%%% common noun
 
+%%%%% common noun
 common_noun(bank, X) :- bank(X).
 common_noun(city, X) :- city(X).
 common_noun(country, X) :- country(X).
 common_noun(man, X) :- man(X).
 common_noun(woman, X) :- woman(X).
-%?
 common_noun(owner, X) :- created(_, X, _, _, _).
 common_noun(person, X) :- person(X).
 common_noun(account, X) :- account(X, _, _, _).
@@ -161,8 +142,8 @@ common_noun(american, X) :- adjective(american, X), person(X).
 common_noun(canadian, X) :- adjective(canadian, X), person(X).
 common_noun(british, X) :- adjective(british, X), person(X).
 
-%%%%% proper noun
 
+%%%%% proper noun
 proper_noun(X) :- person(X).
 proper_noun(X) :- bank(X).
 proper_noun(X) :- account(X,_,_,_).
@@ -170,72 +151,67 @@ proper_noun(X) :- country(X).
 proper_noun(X) :- city(X).
 proper_noun(X) :- number(X).
 
+
 %%%%% adjectives
+adjective(canadian, City) :- location(City, canada).
+adjective(american, City) :- location(City, usa).
+adjective(british, City) :- location(City, united_kingdom).
 
-
-adjective(canadian,City) :- location(City, canada).
-adjective(american,City) :- location(City, usa).
-adjective(british,City) :- location(City, united_kingdom).
-
-adjective(canadian, Bank) :- location(Bank,City), location(City, canada).
-adjective(american, Bank) :- location(Bank,City), location(City, usa).
-adjective(british, Bank) :-  location(Bank,City), location(City, united_kingdom).
+adjective(canadian, Bank) :- location(Bank, City), location(City, canada).
+adjective(american, Bank) :- location(Bank, City), location(City, usa).
+adjective(british, Bank) :-  location(Bank, City), location(City, united_kingdom).
 
 adjective(canadian, Person) :-  lives(Person, City), location(City, canada). 
 adjective(american, Person) :-  lives(Person, City), location(City, usa). 
-adjective(british, Person) :-  lives(Person, City), location(City, united_kingdom). 
+adjective(british, Person) :-  lives(Person, City), location(City, united_kingdom).
 
-adjective(canadian,Account) :- account(Account,_,Bank,_), adjective(canadian, Bank ).
-adjective(american,Account) :- account(Account,_,Bank,_), adjective(american, Bank).
-adjective(british,Account) :-  account(Account,_,Bank,_), adjective(british, Bank).
+adjective(canadian, Account) :- account(Account, _, Bank, _), adjective(canadian, Bank).
+adjective(american, Account) :- account(Account, _, Bank, _), adjective(american, Bank).
+adjective(british, Account) :- account(Account, _, Bank, _), adjective(british, Bank).
 
-adjective(local, X) :-  adjective(canadian, X).
+adjective(local, X) :- adjective(canadian, X).
 adjective(foreign, X) :- adjective(american, X).
 adjective(foreign, X) :- adjective(british, X).
-
 
 adjective(female, X) :- woman(X).
 adjective(male, X) :- man(X).
 
-adjective(small, A) :- account(A,_,_,Balance), Balance < 1000.
-adjective(large, A) :- account(A,_,_,Balance), Balance > 10000.
-adjective(medium, A) :- account(A,_,_,Balance), Balance >= 1000, Balance =< 10000.
+adjective(small, A) :- account(A, _, _, Balance), Balance < 1000.
+adjective(large, A) :- account(A, _, _, Balance), Balance > 10000.
+adjective(medium, A) :- account(A, _, _, Balance), Balance >= 1000, Balance =< 10000.
 
 adjective(small, B) :- number(B), B < 1000.
 adjective(large, B) :- number(B), B > 10000.
 adjective(medium, B) :- number(B), B >= 1000, B =< 10000.
 
-
-
-adjective(new, A) :- account(A,_,_,_), created(A,_,_,_,2024).
+adjective(new, A) :- created(A, _, _, _, 2024).
 adjective(recent, A) :- adjective(new, A).
-adjective(old, A) :- account(A,_,_,_), created(A,_,_,_,Y), Y < 2024.
+adjective(old, A) :- created(A, _, _, _, Y), Y < 2024.
 
 
 %%%%% Prepositions
-preposition(of, X, Y) :- account(X,Y,_,_).    % X is account of owner Y
-preposition(of, X, Y) :- account(_,Y,_,X).    % X is balance of owner Y
-preposition(of, X, Y) :- account(Y,_,_,X).    % X is balance of account Y
-preposition(of, Owner, Account) :- account(Account, Owner, _, _).
+preposition(of, X, Y) :- account(X, Y, _, _). 	% X is account of owner Y
+preposition(of, X, Y) :- account(Y, X, _, _).	% Y is account of owner X
+preposition(of, X, Y) :- account(_, Y, _, X).   % X is balance of owner Y
+preposition(of, X, Y) :- account(Y, _, _, X).   % X is balance of account Y
 
-preposition(from, X, Y) :- lives(X,Y).        % X is person from city Y
-preposition(from, X, Y) :- lives(X, City), location(City, Y).
+preposition(from, X, Y) :- lives(X, Y).       	% X is person from city Y
+preposition(from, X, Y) :- lives(X, City), location(City, Y).	% X is person from country Y
 
-preposition(in, X, Y) :- account(X,_,Y,_).    % X is account in bank Y
-preposition(in, X, Y) :- location(X,Y).       % X is city/bank in country/city Y
-preposition(in, X, Y) :- location(X, City), location(City, Y).
+preposition(in, X, Y) :- account(X, _, Y, _).   % X is account in bank Y
+preposition(in, X, Y) :- location(X, Y).       	% X is city/bank in country/city Y
+preposition(in, X, Y) :- location(X, City), location(City, Y).	% X is bank in country Y
 
-preposition(with, Bank, Account) :- account(Account,_,Bank,_).
-preposition(with, Person, Account) :-          % Person with Account (direct relationship)
-    account(Account, Person, _, _).
-preposition(with, Account1, Account2) :-       % Account1 with Account2 (same bank)
-    account(Account1, _, Bank, _),
-    account(Account2, _, Bank, _),
-    Account1 \= Account2.
-preposition(with, Person, Account2) :-         % Person with Account2 (shared bank)
-    account(Account1, Person, Bank, _),        % Person has Account1
-    account(Account2, _, Bank, _),             % Account2 is in same bank
-    Account1 \= Account2. 
+preposition(with, Bank, Account) :- account(Account, _, Bank, _).
+preposition(with, Person, Account) :- account(Account, Person, _, _).	% Person with Account (direct relationship)
+
+% Account1 with Account2 (same bank)
+preposition(with, Account1, Account2) :- account(Account1, _, Bank, _), account(Account2, _, Bank, _), not Account1 = Account2.
+
+preposition(with, Person, Account) :-	  % More than 1 account at the same bank
+    account(Account, Person, Bank, _),    % Person has Account
+    account(Account2, Person, Bank, _),   % Person also has Account2 at the same bank
+    not Account1 = Account2.
 
 
 %%%%% SECTION: parser 10 10 10 10 / 11 11 11 11
